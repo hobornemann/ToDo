1ddebfe1311ade1c1b350025e7dff38b60854b5e
Hans-Olov <hobornemann@gmail.com>
2024-02-20 18:58:25 +0100

    main.ts updated

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
+//============================================================
+// TODO-LIST 
+//============================================================
+
+//-----------------------------------------
+// IMPORTS  
+//-----------------------------------------
+import type { TodoList } from "./types/todo.d.ts";
 import { v4 as uuidv4 } from 'uuid';
-import { TodoItem, TodoList } from './types/todo';
-import { renderTodoItems } from './modules/todoListMain.ts';
-import { getTodoList, renderTodoListExclTodoItems } from './modules/todoListHeader.ts';
-import { getTodoItemsFromLocalStorage } from './modules/localStorage.ts';
-import { introductionHtml} from './modules/localStorage.ts';
-import {addDragAndDropEventListenerToTodoListMain} from './modules/drag-and-drop.ts'
-
-// Generate a UUID
-const myUUID: string = uuidv4();
-console.log(myUUID);
-
+import { renderTodoListHeaderElement, addEventListenersToTodoListHeaderElement} from './modules/todoListHeader.ts'
+import { renderTodoListMainElement, addEventListenersToTodoListMainElement} from './modules/todoListMain.ts'
+import { renderTodoListFooterElement, addEventListenersToTodoListFooterElement} from './modules/todoListFooter.ts'
 
 
 
@@ -18,74 +18,123 @@ console.log(myUUID);
 //------------------------------------------------------------
 
 document.addEventListener("DOMContentLoaded", () => {
-    try{initialiseTodoList();}
+    try{initialiseTodoListAndElement();}
     catch(error){console.log("Error: ", error);}
 });
 
 
-function initialiseTodoList(){
+//-----------------------------------------
+// INITIALISE  
+//-----------------------------------------
 
-    const todoList: TodoList | undefined = getTodoList();
-        if(todoList){
-            initialiseTodoListHeader();
-            initialiseTodoListMain();
-            initialiseTodoListFooter();    
+export function initialiseTodoListAndElement(){
+
+    const todoList: TodoList | undefined = getTodoListFromLocalStorage();
+    const todoListElement: HTMLElement | null = document.querySelector(".todo-list")
+        if(todoList && todoListElement){
+            renderTodoListElement(todoList, todoListElement);
+            addEventListenersToTodoListHeaderElement(todoListElement);
+            addEventListenersToTodoListMainElement(todoListElement);
+            addEventListenersToTodoListFooterElement(todoListElement);    
         } else {
-          console.log("getTodoList() results in undefined");
+            console.log("getTodoList() results in undefined");
         }
-   
-    // initialiseTodoListHeader
-        // renderHtmlForTodoListHeader
-        // addEventListenersToButtonsInTodoListHeader
-    // initialiseTodoListMain
-        // renderHtmlForTodoListMain
-    // 
-}
-
-function initialiseTodoListHeader(todoList: TodoList):void{
-    renderHtmlForTodoListHeader(todoList);
-    addEventListenersToButtonsOfTodoListHeader(todoList)
 };
 
-function initialiseTodoListMain(todoList: TodoList):void{
-    renderHtmlForTodoListMain(todoList);
-    addEventListenersToButtonsOfTodoListMain(todoList);
-    addEventListenerForDragAndDropOnTodoListMain(todoList);
-};
 
-function initialiseTodoListFooter(todoList: TodoList):void{
-    renderHtmlForTodoListFooter(todoList);
-    addEventListenersToButtonsOfTodoListFooter(todoList);
-};
-
-/* renderTodoListExclTodoItems();
-renderTodoItems(allTodoItems);
-addDragAndDropEventListenersToEachTodoItem();
- */
-
-
-
-const addTodoItemBtn: HTMLButtonElement = document.querySelector(".add-todo-item-btn") as HTMLButtonElement;
-addTodoItemBtn?.addEventListener('click', function() {
+function renderTodoListElement(todoList: TodoList, todoListElement: HTMLElement){
     try{
-        const addTodoItemDescriptionInput: HTMLInputElement = document.querySelector(".add-todo-item-description-input") as HTMLInputElement;
-        const myUUID: string = uuidv4();
-        let todoItem: TodoItem = {
-            id: "todo-item-"+ myUUID,
-            description: addTodoItemDescriptionInput.innerText,
-            isDone: false
-        } 
-
-        renderTodoItem(todoItem);
-        
+        const todoListHeaderElement = todoListElement.querySelector(".todo-list-header") as HTMLElement
+        const todoListMainElement = todoListElement.querySelector(".todo-list-main")  as HTMLUListElement
+        const todoListFooterElement = todoListElement.querySelector(".todo-list-footer")  as HTMLElement
+        if(todoListHeaderElement && todoListMainElement && todoListFooterElement){
+            renderTodoListHeaderElement(todoList, todoListHeaderElement);
+            renderTodoListMainElement(todoList, todoListMainElement);
+            renderTodoListFooterElement(todoList, todoListFooterElement);
+        } else {
+            const errorMessage = "At least one of the following HTML-elements cannot be found: todo-list-header, todo-list-main and/or todo-list-footer."
+            console.error(errorMessage);
+            throw new Error(errorMessage)            
+        }
     }
     catch(error){
-
+        console.error("Error:", error);
+        throw error; 
     }
-});
+};
+
+
+//------------------------------------------------------
+// TS-OBJECT - CREATE, GET (READ), UPDATE, DELETE, ADD 
+//------------------------------------------------------
+
+export function createNewTodoList(): TodoList{
+    const todoList: TodoList = {
+        id: uuidv4(),
+        title: "",
+        todoItems: [],
+    }
+    return todoList
+};
+
+
+export function getTodoListFromLocalStorage(): TodoList | undefined {
+    try{
+        let todoList: TodoList;
+        const todoListJson = localStorage.getItem('todoList');
+        if(todoListJson){
+            todoList = JSON.parse(todoListJson)
+        } else {
+            todoList = createNewTodoList()
+        }
+        return todoList
+    }
+    catch(error){
+        console.log("Error: ", error)
+    }
+};
+
+
+export function updateTodoListInLocalStorage(todoList: TodoList){
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+};
+
+/* export function deleteTodoList(){} */  // ROADMAP
+/* export function addTodoListToWindow(){} */  // ROADMAP
 
 
 
+//-------------------------------------------------------------
+// HTML/DOM ELEMENT - CREATE, GET (READ), UPDATE, DELETE, ADD 
+//-------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------
+// OLD CODE
+//----------------------------------------------------------
+
+/* import { v4 as uuidv4 } from 'uuid'; */
+
+/* // TODO: delete the below code?
+// Generate a UUID
+const myUUID: string = uuidv4();
+console.log(myUUID); */
 
 /* const statusOfTodoItemBtn = todoItemElement.querySelector(".status-of-todo-item-btn") as HTMLButtonElement;
 const statusTodoOfTodoItemImg = todoItemElement.querySelector(".status-todo-of-todo-item-img") as HTMLImageElement;
@@ -116,3 +165,22 @@ if(obj.action === "Status"){
     //       
 }
  */
+
+
+
+/* 
+
+function initialiseTodoListMainElement(todoList: TodoList):void{
+    addEventListenersToButtonsOfTodoListMain(todoList);
+    addEventListenerForDragAndDropOnTodoListMain(todoList);
+};
+
+function initialiseTodoListFooterElement(todoList: TodoList):void{
+    addEventListenersToButtonsOfTodoListFooter(todoList);
+}; */
+
+/* renderTodoListExclTodoItems();
+renderTodoItems(allTodoItems);
+addDragAndDropEventListenersToEachTodoItem();
+ */
+
