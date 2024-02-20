6a558917c5e4bcc9f8c5caee05ca1edd67aa0e8c
Hans-Olov <hobornemann@gmail.com>
2024-02-20 18:01:27 +0100

    todoListHeaderElement

--- src/modules/todoListHeader.ts
+++ src/modules/todoListHeader.ts
@@ -2,67 +2,71 @@
 // TODO-LIST HEADER 
 //============================================================
 
+//-----------------------------------------
+// IMPORTS   
+//-----------------------------------------
 
-import { TodoItem, TodoList } from "../types/todo";
-import { v4 as uuidv4 } from 'uuid';
-import { introductionHtml } from "./localStorage";}
-
-
-// CREATE, GET, UPDATE, DELETE, ADD 
-
-export function createTodoList(): TodoList{
-    const title = "";
-    const todoItems: TodoItem[] = [];
-    const todoList: TodoList = {
-        id: uuidv4(),
-        title: title,
-        introductionHtml: introductionHtml,
-        todoItems: todoItems,
-    }
-    return todoList
-};
-
-export function getTodoList(): TodoList | undefined {
-    try{
-        let todoList: TodoList;
-        const todoListJson = localStorage.getItem('todoList');
-        if(todoListJson){
-            todoList = JSON.parse(todoListJson)
-        } else {
-            todoList = createTodoList()
-        }
-        return todoList
-    }
-    catch(error){
-        console.log("Error: ", error)
-    }
-};
-
-export function updateTodoList(){
-//TODO:
-};
-
-/* export function deleteTodoList(){} */  // ROADMAP
-/* export function addTodoListToWindow(){} */  // ROADMAP
+import {TodoList} from '../types/todo'
 
 
+//-----------------------------------------
+// INITIALISE - RENDER HTML/DOM-ELEMENT   
+//-----------------------------------------
 
-
-// RENDER HTML, ADD EVENT-LISTENERS 
-
-export function renderTodoListExclTodoItems(){
-
-    
-    renderTodoListEditButtonFunctionality();
-    renderTodoListInfoButtonFunctionality();
-    renderTodoListDeleteButtonFunctionality();
-
+export function renderTodoListHeaderElement(todoList: TodoList, todoListHeaderElement: HTMLElement){
+    let html: string = `
+        <h3 class="todo-list-heading" contenteditable="false">My ToDo List</h3>
+            <button class="edit-todo-list-heading-btn button">
+                <img class="todo-item-img icon" src="/icons/pencil-svgrepo-com.svg" title="Change the name of the todo list" alt="edit-icon: Change the name of the todo list">
+            </button>
+            <button class="info-about-todo-list-btn button">
+                <img class="todo-item-img icon" src="/icons/info-borderless-svgrepo-com.svg" title="Intro to the todo list" alt="info-icon: Intro to the todo list">
+            </button>
+            <div class="info-dialog">
+                <h4>Welcome to the ToDo list!</h4>
+                <br>
+                <li class="bold">ToDo List</li>
+                <li>Edit: Edit the title of the todo list by clicking the pencil icon next to it. </li>
+                <li>Info: Click the info-button to see this information.</li>
+                <li>Delete: Delete all todo items by clicking the trash can in the upper right hand corner.</li>
+                <br>
+                <li class="bold">ToDo Items</li>
+                <li>Click & drag: Change priority of your todos by clicking & dragging them.</li>
+                <li>Edit: Edit todo-text or title of todo list by clicking the pencil icon.</li>
+                <li>Check: When you have completed the todo and want to keep it further down in the todo list, click the check-icon.</li>
+                <li>Delete: Click the trash can of the todo item, if you want to delete the item.</li>                
+                <br>
+                <button class="close-info-dialog-btn button">&nbsp; Close &nbsp; </button>
+            </div>
+            <div class="info-dialog-overlay"></div>
+            <button class="delete-all-todo-items-btn button">
+                <img class="delete-all-todo-items-img icon" src="/icons/trash-svgrepo-com.svg" title="Deletes all items in the todo list" alt="delete-icon: Deletes all items in the todo list" >
+            </button>
+    `;
+    todoListHeaderElement.innerHTML = html;
 }
 
-function renderTodoListEditButtonFunctionality(){
+
+//-----------------------------------------
+// INITIALISE - ADD EVENT-LISTENERS   
+//-----------------------------------------
+
+export function addEventListenersToTodoListHeaderElement(todoListElement: HTMLElement):void {  
+    addEventListenerToEditButtonInTodoListHeaderElement(todoListElement);
+    addEventListenerToInfoButtonInTodoListHeaderElement(todoListElement);
+    addEventListenerToDeleteTodoItemsButtonInTodoListHeaderElement(todoListElement);
+};
+
+
+//-----------------------------------------
+// ADD EVENT-LISTENER - FUNCTIONS 
+//-----------------------------------------
+
+
+function addEventListenerToEditButtonInTodoListHeaderElement(todoListElement: HTMLElement){
     try {
-        const editTodoListHeadingBtn = document.querySelector('.edit-todo-list-heading-btn') as HTMLElement;
-        const todoListHeading = document.querySelector('.todo-list-heading') as HTMLElement;
+        const editTodoListHeadingBtn = todoListElement.querySelector('.edit-todo-list-heading-btn') as HTMLElement;
+        const todoListHeading = todoListElement.querySelector('.todo-list-heading') as HTMLElement;
 
         editTodoListHeadingBtn?.addEventListener('click', () => {
             const isEditable = todoListHeading?.getAttribute('contenteditable') === 'true';
@@ -80,12 +84,13 @@ function renderTodoListEditButtonFunctionality(){
     }
 };
 
-function renderTodoListInfoButtonFunctionality(){
+
+function addEventListenerToInfoButtonInTodoListHeaderElement(todoListElement: HTMLElement){
     
-    const infoButtons = document.querySelectorAll('.info-about-todo-list-btn');
-    const infoDialogs = document.querySelectorAll('.info-dialog');
-    const closeInfoDialogButtons = document.querySelectorAll('.close-info-dialog-btn');
-    const infoDialogOverlays = document.querySelectorAll('.info-dialog-overlay');
+    const infoButtons = todoListElement.querySelectorAll('.info-about-todo-list-btn');
+    const infoDialogs = todoListElement.querySelectorAll('.info-dialog');
+    const closeInfoDialogButtons = todoListElement.querySelectorAll('.close-info-dialog-btn');
+    const infoDialogOverlays = todoListElement.querySelectorAll('.info-dialog-overlay');
     
     infoButtons.forEach((button, index) => {
         button.addEventListener('click', () => {
@@ -102,11 +107,12 @@ function renderTodoListInfoButtonFunctionality(){
     });
 };
 
-function renderTodoListDeleteButtonFunctionality(){
+
+function addEventListenerToDeleteTodoItemsButtonInTodoListHeaderElement(todoListElement: HTMLElement){
     try {
-        const deleteAllTodoItemsButton = document.querySelector(".delete-all-todo-items-btn") as HTMLButtonElement;
+        const deleteAllTodoItemsButton = todoListElement.querySelector(".delete-all-todo-items-btn") as HTMLButtonElement;
         deleteAllTodoItemsButton?.addEventListener('click', () => {
-            const todoItems: NodeListOf<HTMLElement> = document.querySelectorAll('.todo-item') as NodeListOf<HTMLElement>;
+            const todoItems: NodeListOf<HTMLElement> = todoListElement.querySelectorAll('.todo-item') as NodeListOf<HTMLElement>;
             todoItems.forEach(todoItem => {
                 todoItem.remove();
             });
